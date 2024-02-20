be8db69cb55b685744fa5c399e45ac5ffec972f8
psousatv <psousa@taviraverde.pt>
2024-02-20 15:06:01 +0000

    atualização excritório

--- producao/processos/dados/cadastro.php
+++ producao/processos/dados/cadastro.php
@@ -2,82 +2,86 @@
 //session_start();
 include "../../../global/config/dbConn.php";
 
-if (isset($_POST["action"])){
-    if ($_POST["action"] == 'fetch'){
-
-        $order_column = array("proces_estado_nome");
-
-        $main_query = "SELECT * 
+if(isset($_POST["action"]))
+{
+	if($_POST["action"] == 'fetch')
+	{
+		$main_query = "SELECT
+					   proces_cod,
+                       proces_nome,
+                       proces_contrato,
+					   proces_cpv_sigla,
+					   proces_estado,
+                       proces_estado_nome,
+                       YEAR(proces_data_adjudicacao) AS ano_adjudicacao,
+					   ROUND(SUM(proces_val_max),2) AS val_max,
+					   ROUND(SUM(proces_val_adjudicacoes),2) AS val_adjudicacoes,
+                       ROUND(SUM(proces_val_faturacao),2) AS val_faturacao
                        FROM processo ";
-                      
-        $search_query = "WHERE proces_report_valores = 1 AND 
-                         proces_contrato <> 'Qualquer Contrato' AND 
-                         proces_estado_nome = 'Em Curso' AND ";
 
-        if (isset($_POST["search"]["value"])){
-            $search_query .= '(proces_estado_nome LIKE "%'.$_POST["search"]["value"].'%")';
-        }
-
-        $group_by_query = "GROUP BY proces_cod ";
-        $order_by_query = "";
-
-        if(isset($_POST["tabela"])){
-            $order_by_query = 'ORDER BY '.$order_column[$_POST['0']['column']].''.$_POST['tabela']['0']['dir'].'';
-        }
-        else {
-
-            $order_by_query = 'ORDER BY ano_adjudicacao DESC';
-
-        }
-
-        $limit_query = '';
-
-        if ($_POST["lenght"] != 1) {
-
-            $limit_query = 'LIMIT'.$_POST['start'].','.$_POST['lenght'];
-
-        }
-
-        $stmt = $myConn->prepare($main_query .$search_query .$group_by_query .$order_by_query);
-        $stmt->execute();
-        $filtered_rows = $stmt->rowCount();
-
-        $stmt = $myConn->prepare($main_query .$group_by_query);
-        $stmt->execute();
-        $total_rows = $stmt->rowCount();
-
-        $result = $myConn->query($main_query .$search_query .$group_by_query .$order_by_query .$limit_query, PDO::FETCH_ASSOC);
+        $search_query = 'WHERE (proces_cod > 0 AND proces_report_valores = 1 AND
+						 (proces_estado >= 201 AND proces_estado <= 208) OR 
+						 (proces_estado >= 219 AND proces_estado <= 220)) AND ';
         
+        if(isset($_POST["search"]["value"]))
+        {
+			$search_query .= '(proces_cod LIKE "%'.$_POST["search"]["value"].'%" OR
+							   proces_estado_nome LIKE "%'.$_POST["search"]["value"].'%" )';
+		}
+
+		$group_by_query = " GROUP BY proces_contrato, proces_estado ";
+
+		$order_by_query = "";
+		if(isset($_POST["myDataTable"]))
+        {
+			$order_by_query = 'ORDER BY '.$order_column[$_POST['myDataTable']['0']['column']].' '.$_POST['myDataTable']['0']['dir'].' ';
+		}
+		else
+		{
+			$order_by_query = 'ORDER BY proces_contrato, proces_estado ASC ';
+		}
+
+		$limit_query = '';
+
+		if($_POST["length"] != -1){
+			$limit_query = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
+		}
+
+		$statement = $myConn->prepare($main_query . $search_query . $group_by_query . $order_by_query);
+		$statement->execute();
+
+		$filtered_rows = $statement->rowCount();
+		$statement = $myConn->prepare($main_query . $order_by_query);
+		$statement->execute();
+
+		$total_rows = $statement->rowCount();
+		//$result = $myConn->query($main_query . $search_query . $group_by_query . $order_by_query . $limit_query, PDO::FETCH_ASSOC);
+		$result = $myConn->query($main_query . $search_query . $group_by_query . $order_by_query . $limit_query, PDO::FETCH_ASSOC);
         $data = array();
-        foreach($result as $row){
 
-            $sub_array = array();
+		foreach($result as $row)
+		{
+			$sub_array = array();
+            //$sub_array[] = $row['proces_cod'];
+            //$sub_array[] = $row['proces_nome'];
+            $sub_array[] = $row['proces_contrato'];
+            $sub_array[] = $row['proces_estado_nome'];
+			//$sub_array[] = $row['val_max'];
+            $sub_array[] = $row['val_adjudicacoes'];
+            $sub_array[] = $row['val_faturacao'];
 
-            $data = $sub_array;
- 
-        }
+			$data[] = $sub_array;
+		}
 
-        $dados = array(
-            "draw" => intval($_POST["draw"]),
-            "recordsTotal" => $total_rows,
-            "recordFiltered" => $filtered_rows,
-            "dados" => $data
-        );
-        
-        // Set the HTTP Content-Type header to indicate that the response is in JSON format
-        header('Content-Type: application/json');
-        
-        echo json_encode($dados);
+		$output = array(
+			"draw"			=>	intval($_POST["draw"]),
+			"recordsTotal"	=>	$total_rows,
+			"recordsFiltered" => $filtered_rows,
+			"data"			=>	$data
+		);
 
-    }
+		echo json_encode($output);
+	}
 }
 
-
-
-
-
-
-
-
-
-
+?>
--- producao/processos/js/cadastro.js
+++ producao/processos/js/cadastro.js
@@ -1,70 +1,86 @@
+// Cadastro
 
-// Cores a atribuir aos Gráficos
-var cores = ['red', 'blue', 'green', 'purple', 'orange'];
-
-
-$(document).ready(function(){
-
-    fetch_data();
-
-    var grafico;
-
-    function fetch_data(start_date='', end_date=''){
-        var dataTable = $('#tabela').DataTable({
-            "processing":true,
-            "serverSide":true,
-            "tabela":[],
+$(document).ready(
+    function()
+    {
+        fetch_data();
+        var grafico1;
+        var grafico2;
+        function fetch_data()
+        {
+        var dataTable = $('#tabela').DataTable(
+            {
+            //"scrollY": 300,
+            //"paging": false,
+            "processing": true,
+            "serverSide": true,
+            "myDataTable": [],
             "ajax":{
-                "url":"dados/cadastro.php",
-                "type":"POST",
-                "data":{action:'fetch'}
+                url:"dados/cadastro.php",
+                type:"POST",
+                data:{action:'fetch'}
             },
             "columnDefs":[
-                {targets:[36, 39, 29, 33, 38]},
-                [targets[48, 50]]
+                { targets: [2, 3], className: 'dt-body-right', "render": $.fn.dataTable.render.number('.', ',', 2, '') },
+                { targets: [0, 1], className: 'dt-body-left' }
             ],
-            "drawCallback":function(settings){
-                // 
-                var valor_x = [];
-                // adjudicado
-                var valor_y = [];
-                // faturado
-                var valor_y1 = [];
-
-                for (var count = 0; count < settings.aoData.length; count ++){
-                    valor_x.push(settings.aoData[count].aData[0]);
-                    valor_y.push(parseFloat(settings.aoData[count].aData[6]));
-                    valor_y1.push(parseFloat(settings.aoData[count].aData[7]));
+            "drawCallback": function(settings){
+                var tabela_valor_x = [];
+                var tabela_valor_y1 = [];
+                var tabela_valor_y2 = [];
+                var tabela_valor_y3 = [];
+                for(var count = 0; count < settings.aoData.length; count++){
+                tabela_valor_x.push(settings.aoData[count]._aData[0]);
+                tabela_valor_y1.push(parseFloat(settings.aoData[count]._aData[2]));
+                tabela_valor_y2.push(parseFloat(settings.aoData[count]._aData[3]));
+                tabela_valor_y3.push(parseFloat(settings.aoData[count]._aData[4]));
                 }
-
-                var chart_data = {
-                    labels: valor_x,
-                    datasets:[
-                        {
-                            label: '',
-                            backgroubdColor: 'rgba(105,0,132,.2)',
-                            data: valor_y
-                        },
-                        {
-                            label: '',
-                            backgroubdColor: 'rgba(105,0,132,.2)',
-                            data: valor_y
-                        }
-                    ]
+                var chart_data1 = {
+                labels: tabela_valor_x,
+                datasets:[
+                    {
+                    label : 'Previsto',
+                    backgroundColor : 'rgba(105, 0, 132, .2)',
+                    //color : '#fff',
+                    data: tabela_valor_y1
+                    },
+                    {
+                    label : 'Adjudicado',
+                    backgroundColor : 'rgba(200, 99, 132, .7)',
+                    //color : '#fff',
+                    data: tabela_valor_y2
+                    }
+                ]
                 };
-
-                var group_chart = $('#grafico');
-                
-                if (grafico){
-                    grafico.destroy();
+                var group_chart1 = $('#grafico');
+                if(grafico1)
+                {
+                grafico1.destroy();
                 }
-
-                grafico = new chart_data(group_chart, {
-                    type:'line',
-                    data: chart_data
+                grafico1 = new Chart(group_chart1,
+                {
+                type: 'bar',
+                data: chart_data1
                 });
+                var chart_data2 = {
+                labels: tabela_valor_x,
+                datasets:[
+                    {
+                    label : 'Adjudicado',
+                    backgroundColor : 'rgba(105, 0, 132, .2)',
+                    //color : '#fff',
+                    data: tabela_valor_y2
+                    },
+                    {
+                    label : 'Executado',
+                    backgroundColor : 'rgba(200, 99, 132, .7)',
+                    //color : '#fff',
+                    data: tabela_valor_y3
+                    }
+                ]
+                }
             }
-        });
+            });
+        }
     }
-
-});
+    );
