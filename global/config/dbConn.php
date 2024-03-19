32ecd355a0efa1dfb013aae168459797420856a6
blequim76 <blequim76@gmail.com>
2024-03-19 21:00:13 +0000

    conn

--- global/config/dbConn.php
+++ global/config/dbConn.php
@@ -1,8 +1,8 @@
 <?php
 
 //Variaveis conexão ao servidor
-//$myHost = "localhost";
-$myHost = "mdb1";
+$myHost = "localhost";
+//$myHost = "mdb1";
 $user = "tv";
 $password = "taviraverde";
 //Bases de Dados
@@ -16,16 +16,3 @@ if($myConn === false) {
     echo "Ocorreu um erro com a conexão";
     exit();
 }
-
-//PDO - Connection to Postgresql
-//$pgConn = new PDO("pgsql:host=$myHost port=5432 dbname=$database user=$user password=$password");
-//if($pgConn === false) {
-//    echo "Ocorreu um erro com a conexão pdo";
-//    exit();
-//}
-
-//PG_CONNECT - Connection to Postgresql
-//$pgConn = pg_connect("host=$myHost port=5432 dbname=$pgDatabase user=$user password=$password");
-//if(!$pgConn){
-//    die('Ocorreu um erro com a conexão pg_connect');
-//}
