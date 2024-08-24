0b4490a059e87c562176dcdadfc3089599a99b3f
blequim76 <blequim76@gmail.com>
2024-08-24 14:26:48 +0100

    melhoramentos em avisos no módulo de processos

--- producao/processos/dados/processosAvisos.php
+++ producao/processos/dados/processosAvisos.php
@@ -2,20 +2,82 @@
 //session_start();
 include "../../../global/config/dbConn.php";
 
-$faseProcessos = $_GET['faseProcessos'];
+//$faseProcessos = $_GET['faseProcessos'];
 
-$query = 'SELECT proces_check, proces_padm, proces_cod, proces_sub, proces_nome, proces_estado_data, descr_fase_processo
+$query = 'SELECT proces_check, proces_nome, proces_estado, proces_estado_data, proces_estado_nome, ent_nome
           FROM processo
-          INNER JOIN descritivos d ON descr_cod = proces_estado
-          WHERE descr_fase_processo ="'.$faseProcessos.'"
-          ORDER BY proces_cod, proces_estado_data';
+          INNER JOIN entidade ent ON ent_cod = proces_ent_cod
+          ORDER BY proces_cod, proces_estado_data, proces_estado';
 
+//WHERE descr_fase_processo ="'.$faseProcessos.'"
 
 $stmt = $myConn->query($query);
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach($data as $row) {
-  echo  '
-    <div class="small text-left">'.$row["proces_nome"].'</div>
+
+
+//foreach($data as $row) {
+//  echo  '
+//    <div class="small text-left">'.$row["proces_nome"].'</div>
+//    ';
+//};
+
+echo '  
+<!-- 1.ª Coluna -->
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header text-white" style="background-color: green;"><i class="fa-duotne fa-solid fa-person-digging"></i> Em Curso</div>
+        <div class="card-body" id="processosFaseCurso">
+          <table class="table table-striped small">
+        ';
+        foreach($data as $row) {
+          if($row['proces_estado'] == '208'){
+          echo  '
+            <tr>
+              <td>'.$row["proces_estado_nome"].'_'.$row["proces_nome"].' ('.$row["ent_nome"].')</td>
+            </tr>
+          ';}};
+  echo '
+          </table>
+        </div>
+      </div>
+    </div>
+    <!-- 2.ª Coluna -->
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header text-white" style="background-color: blue;"><i class="fas fa-table mr-1"></i> Em Contratação ou Adjudicado</div>
+        <div class="card-body" id="processosFaseConcurso">
+          <table class="table table-striped small">
+        ';
+        foreach($data as $row) {
+          if($row['proces_estado'] == '205' OR $row['proces_estado'] == '206'){
+          echo  '
+            <tr>
+              <td>'.$row["proces_estado_nome"].'_'.$row["proces_nome"].' ('.$row["ent_nome"].')</td>
+            </tr>
+          ';}};
+  echo '
+          </table>
+        </div>
+      </div>
+    </div>
+    <!-- 3.ª Coluna -->
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header text-white" style="background-color: red;"><i class="fas fa-table mr-1"></i> Em Consulta de Mercado</div>
+        <div class="card-body" id="processosFaseConsulta">
+          <table class="table table-striped small">
+        ';
+        foreach($data as $row) {
+          if($row['proces_estado'] == '203'){
+          echo  '
+            <tr>
+              <td>'.$row["proces_estado_nome"].'_'.$row["proces_nome"].' ('.$row["ent_nome"].')</td>
+            </tr>
+          ';}};
+echo '
+            </table>
+        </div>
+      </div>
+    </div>
     ';
-};
--- producao/processos/js/processos.js
+++ producao/processos/js/processos.js
@@ -13,18 +13,17 @@ function mostraResultados(){
 };
 
 // Avisos
-function avisos(faseProcessos){
+function avisos(){
   var xmlhttp = new XMLHttpRequest();
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("processosFaseCurso").innerHTML = this.responseText;
-      document.getElementById("processosFaseConcurso").innerHTML = this.responseText;
+      document.getElementById("Avisos").innerHTML = this.responseText;
     }
   }
 
  
-  xmlhttp.open("GET","dados/processosAvisos.php?faseProcessos="+'208',true);
+  xmlhttp.open("GET","dados/processosAvisos.php");
   xmlhttp.send();
 
 };
