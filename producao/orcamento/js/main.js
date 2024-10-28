8b0d5f950cc4fc3332c8fdc14e81266a98f9040d
psousatv <psousa@taviraverde.pt>
2024-10-28 13:52:43 +0000

    Cartões de orçamento separados por tipologia

--- producao/orcamento/js/main.js
+++ producao/orcamento/js/main.js
@@ -28,11 +28,10 @@ $.ajax(
             })
 
            
-            // ** Cartões
-            var container = document.getElementById('cartoes');
-            container.innerHTML = "";
+            // ** Investimentos
+            var containerInvestimentos = document.getElementById('cartoesInvestimentos');
+            containerInvestimentos.innerHTML = "";
             data.forEach((result) => {
-            // Create card element
             
             var classeCartao = ''
             var iconeCartao = ''
@@ -54,27 +53,72 @@ $.ajax(
             const card = document.createElement('div');
             card.classList = 'card-body';
             
-            
-            var cartoes = `
-                
-                <div onclick="orcamentoRedirected('${result["cod"]}')" class="card col-md-3 ${classeCartao}">
-                    <div class="d-flex justify-content-between px-md-1">
-                        <div class="text-end">
-                            <p class="mb-0 small text-white">${result["item"]}</p>
-                            <!--Faturado-->
-                            <h3 class="text-white">${Number(result["faturado"]).toLocaleString('pt')}€<span class="h6">- ${result["realizado"]}%</span></h3>
-                            <!--Orçamento-->
-                            <h6 class="text-white">${Number(result["orcamento"]).toLocaleString('pt')}€<span class="h6"> </span></h6>
-                        </div>
-                        <div class="align-self-center">
-                            <i class="fas ${iconeCartao} text-white fa-3x"></i>
+            if(result["tipo"] == 'Investimento'){
+                var cartoesIvestimentos = `     
+                    <div onclick="orcamentoRedirected('${result["cod"]}')" class="card col-md-3 ${classeCartao}">
+                        <div class="d-flex justify-content-between px-md-1">
+                            <div class="text-end">
+                                <p class="mb-0 small text-white">${result["item"]}</p>
+                                <!--Faturado-->
+                                <h3 class="text-white">${Number(result["faturado"]).toLocaleString('pt')}€<span class="h6">- ${result["realizado"]}%</span></h3>
+                                <!--Orçamento-->
+                                <h6 class="text-white">${Number(result["orcamento"]).toLocaleString('pt')}€<span class="h6"> </span></h6>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="fas ${iconeCartao} text-white fa-3x"></i>
+                            </div>
                         </div>
                     </div>
-                </div>
-            `;
+                `;
             // Append newyly created card element to the container
-            container.innerHTML += cartoes;
-            });
+            containerInvestimentos.innerHTML += cartoesIvestimentos;
+            }});
+
+            // ** Investimentos
+            var containerInvestimentos = document.getElementById('cartoesGastos');
+            containerInvestimentos.innerHTML = "";
+            data.forEach((result) => {
+            
+            var classeCartao = ''
+            var iconeCartao = ''
+
+            if (result["realizado"] < 10) {
+                var classeCartao = 'bg-danger';
+                var iconeCartao = 'fa-thumbs-down'
+            } else if (result["realizado"] >= 10 & result["realizado"]< 35){
+                var classeCartao = 'bg-warning';
+                var iconeCartao = 'fa-warning'
+            } else if (result["realizado"] >= 35 & result["realizado"] < 75){
+                var classeCartao = 'bg-primary';
+                var iconeCartao = 'fa-cogs'
+            } else {
+                var classeCartao = 'bg-success';
+                var iconeCartao = 'fa-smile'
+            };
+
+            const card = document.createElement('div');
+            card.classList = 'card-body';
+            
+            if(result["tipo"] == 'Gastos'){
+                var cartoesGastos = `     
+                    <div onclick="orcamentoRedirected('${result["cod"]}')" class="card col-md-3 ${classeCartao}">
+                        <div class="d-flex justify-content-between px-md-1">
+                            <div class="text-end">
+                                <p class="mb-0 small text-white">${result["item"]}</p>
+                                <!--Faturado-->
+                                <h3 class="text-white">${Number(result["faturado"]).toLocaleString('pt')}€<span class="h6">- ${result["realizado"]}%</span></h3>
+                                <!--Orçamento-->
+                                <h6 class="text-white">${Number(result["orcamento"]).toLocaleString('pt')}€<span class="h6"> </span></h6>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="fas ${iconeCartao} text-white fa-3x"></i>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            // Append newyly created card element to the container
+            containerInvestimentos.innerHTML += cartoesGastos;
+            }});
         }
     );
 
