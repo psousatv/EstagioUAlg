4ce9b379ef7d38bc337d705c724fdd042e47edbb
psousatv <psousa@taviraverde.pt>
2026-03-03 10:47:18 +0000

    indexação do código 14 para a execução do processo

--- producao/processos/dados/processoHistorico.php
+++ producao/processos/dados/processoHistorico.php
@@ -19,11 +19,11 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 function obterMovimentosDaFase($faseDesejada) {
     $fases = [
         "enquadramento" => [0, 1, 2, 3, 53, 54],
-        "concurso" => [4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 51, 52, 80, 81, 82],
+        "concurso" => [4, 5, 6, 7, 8, 10, 11, 12, 13, 51, 52, 80, 81, 82],
         "contrato" => [16, 17, 19, 40],
         "garantias" => [40, 41, 42, 43, 44],
         "preparacao_execucao" => [60, 61, 62, 63, 64],
-        "execucao" => [9, 18, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 96, 98]
+        "execucao" => [9, 14, 18, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 96, 98]
     ];
 
     return $fases[$faseDesejada] ?? [];
