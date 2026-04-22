b83f5d1f9851ad555c73f44193911d435aee3c43
psousatv <psousa@taviraverde.pt>
2026-04-22 14:44:14 +0000

    testes

--- producao/metricas/js/metricasIndicadores.js
+++ producao/metricas/js/metricasIndicadores.js
@@ -4,21 +4,21 @@ const ctx = document.getElementById('volumeChart');
           new Chart(ctx, {
             type: 'bar',
             data: {
-              labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+              labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
               datasets: [
                 {
-                  label: 'Volume Captado',
-                  data: [120, 130, 125, 140, 135, 150],
+                  label: 'Previsto',
+                  data: [199, 210, 234, 347, 341, 341, 390, 227, 235, 227, 235],
                   backgroundColor: 'rgba(54, 162, 235, 0.6)'
                 },
                 {
-                  label: 'Volume Faturado',
-                  data: [90, 95, 92, 100, 98, 105],
+                  label: 'Faturado',
+                  data: [220, 230, 50],
                   backgroundColor: 'rgba(75, 192, 192, 0.6)'
                 },
                 {
-                  label: 'Perdas (%)',
-                  data: [25, 27, 26, 29, 28, 30],
+                  label: 'Realização (%)',
+                  data: [110, 110, 77],
                   type: 'line',
                   borderColor: 'red',
                   yAxisID: 'y1'
@@ -30,7 +30,7 @@ const ctx = document.getElementById('volumeChart');
               scales: {
                 y: {
                   beginAtZero: true,
-                  title: { display: true, text: 'Volume' }
+                  title: { display: true, text: 'Milhares de Euros' }
                 },
                 y1: {
                   position: 'right',
